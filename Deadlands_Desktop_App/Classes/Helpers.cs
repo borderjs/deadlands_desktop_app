54bff90c2de32670fde0d5378a0fb2ff20895db7
Jonathan Borders <user1@gitlab.rocks>
2018-03-18 13:00:35 -0400

    Good Start

---
+++ Deadlands_Desktop_App/Classes/Helpers.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Deadlands_Desktop_App.Classes
+{
+    public static class Helpers
+    {
+        public static int GetRandomNumber(Random rnd, int min, int max)
+        {
+            int result = rnd.Next(min, max + 1); // creates a number between min and max
+            return result;
+        }
+
+        public static string GetLocation(int result, int raises, int evenOdd)
+        {
+            if (raises == 0)
+            {
+                return ChartLookup(result, evenOdd);
+            }
+            else
+            {
+                if (result + raises > 20)
+                {
+                    return ChartLookup(result - raises, evenOdd);
+                }
+                else if (result - raises < 1)
+                {
+                    return ChartLookup(result + raises, evenOdd);
+                }
+                else
+                {
+                    return ChartLookup(result - raises, evenOdd) + " / " + ChartLookup(result + raises, evenOdd);
+                }
+            }
+        }
+
+        private static string ChartLookup(int result, int evenOdd)
+        {
+            switch (result)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    if (evenOdd == 1)
+                    {
+                        return "Left Leg";
+                    }
+                    else
+                    {
+                        return "Right Leg";
+                    }
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    return "Guts";
+                case 10:
+                    return "Gizzards (+1 Die)";
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                    if (evenOdd == 1)
+                    {
+                        return "Left Arm";
+                    }
+                    else
+                    {
+                        return "Right Arm";
+                    }
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                    return "Guts";
+                case 20:
+                    return "Head (+2 Dice)";
+            }
+            return string.Empty;
+        }
+
+        internal static List<int> RollDice(int numberOfDice, int dieValue)
+        {
+            Random rnd = new Random();
+            List<int> results = new List<int>();
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                int total = 0;
+                int roll = rnd.Next(1, dieValue+1);
+                total += roll;
+                while (roll == dieValue)
+                {
+                    roll = rnd.Next(1, dieValue + 1);
+                    total += roll;
+                }
+                results.Add(total);
+            }
+            return results;
+        }
+
+        internal static string ParseResults(List<int> results, ref string values)
+        {
+            values =" (";
+            for (int i = results.Count - 1; i >= 0; i--)
+            {
+                values += results[i] + ", ";
+            }
+            values = values.Substring(0, values.Length - 2) + ")";
+            return results.Where(z => z == 1).Count() > results.Count / 2 ? "Bust!" : results[results.Count - 1] + string.Empty;
+        }
+    }
+}
--- Deadlands_Desktop_App/MainWindow.xaml.cs
+++ Deadlands_Desktop_App/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Deadlands_Desktop_App.Classes;
 using MahApps.Metro.Controls;
 
 namespace Deadlands_Desktop_App
@@ -21,9 +23,122 @@ namespace Deadlands_Desktop_App
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnHit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int hitLocation = Helpers.GetRandomNumber(rnd, 1, 20);
+            this.PopulateChart(hitLocation);
+            
+        }
+
+        private void BtnMinus2_Click(object sender, RoutedEventArgs e)
+        {
+            int hitLocation = Helpers.GetRandomNumber(rnd, 1, 20);
+            hitLocation = (hitLocation - 2) < 1 ? 1 : hitLocation - 2;
+            this.PopulateChart(hitLocation);
+        }
+
+        private void BtnPlus2_Click(object sender, RoutedEventArgs e)
+        {
+            int hitLocation = Helpers.GetRandomNumber(rnd, 1, 20);
+            hitLocation = (hitLocation + 2) > 20 ? 20 : hitLocation + 2;
+            this.PopulateChart(hitLocation);
+        }
+
+        private void BtnPlus6_Click(object sender, RoutedEventArgs e)
+        {
+            int hitLocation = Helpers.GetRandomNumber(rnd, 1, 20);
+            hitLocation = (hitLocation + 6) > 20 ? 20 : hitLocation + 6;
+            this.PopulateChart(hitLocation);
+        }
+
+        private void BtnPlus4_Click(object sender, RoutedEventArgs e)
+        {
+            int hitLocation = Helpers.GetRandomNumber(rnd, 1, 20);
+            hitLocation = (hitLocation + 4) > 20 ? 20 : hitLocation + 4;
+            this.PopulateChart(hitLocation);
+        }
+
+        private void PopulateChart(int hitLocation)
+        {
+            int evenOdd = Helpers.GetRandomNumber(rnd, 1, 2);
+            int armor = Helpers.GetRandomNumber(rnd, 1, 2);
+            LblResult.Content = "Result: " + hitLocation;
+            TxtLocation.Text = Helpers.GetLocation(hitLocation, 0, evenOdd);
+            Txt1Raise.Text = Helpers.GetLocation(hitLocation, 1, evenOdd);
+            Txt2Raises.Text = Helpers.GetLocation(hitLocation, 2, evenOdd);
+            Txt3Raises.Text = Helpers.GetLocation(hitLocation, 3, evenOdd);
+            if (armor == 1)
+            {
+                TxtArmor.Text = "Deflected";
+            }
+            else
+            {
+                TxtArmor.Text = "Bustin' Through";
+            }
+        }
+
+        private void BtnCustomRoll_Click(object sender, RoutedEventArgs e)
+        {
+            string roll = TxtCustomRoll.Text.ToLower().Trim();
+            string [] parts = roll.Split('d');
+            if (parts.Count() == 2)
+            {
+                int numberOfDice;
+                int dieValue;
+                if (Int32.TryParse(parts[0], out numberOfDice))
+                {
+                    if (Int32.TryParse(parts[1], out dieValue))
+                    {
+                        if (numberOfDice >= 1 && dieValue > 1)
+                        {
+                            List<int> results = Helpers.RollDice(numberOfDice, dieValue);
+                            results.Sort();
+                            string values = string.Empty;
+                            string resultString = Helpers.ParseResults(results, ref values);
+                            DateTime time = DateTime.Now;
+                            CheckBlockCustomRollResult();
+                            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(values)));
+                            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(resultString) { FontWeight = FontWeights.Bold }));
+                            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(time.ToString("HH:mm:ss") + " - Rolling " + roll + "! - ")));
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnRolLScart_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            int numDice = Convert.ToInt32(button.Tag);
+            List<int> results = Helpers.RollDice(numDice, 6);
+            results.Sort();
+            string values = string.Empty;
+            string resultString = Helpers.ParseResults(results, ref values);
+            DateTime time = DateTime.Now;
+            CheckBlockCustomRollResult();
+            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(values)));
+            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(resultString) { FontWeight = FontWeights.Bold }));
+            BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(time.ToString("HH:mm:ss") + " - Rolling Scart " + numDice + "d6! - ")));
+        }
+
+        private void CheckBlockCustomRollResult()
+        {
+            if (BlockCustomRollResult.Inlines.FirstInline == null)
+            {
+                BlockCustomRollResult.Inlines.Add(new Run(Environment.NewLine));
+            }
+            else
+            {
+                BlockCustomRollResult.Inlines.InsertBefore(BlockCustomRollResult.Inlines.FirstInline, (new Run(Environment.NewLine)));
+            }
         }
     }
 }
